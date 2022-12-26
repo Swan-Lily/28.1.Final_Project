db3d2b0fefc3d9a525c68da324c2ddaa9c0dfd76
Swan-Lily <Liliashad@mail.ru>
2022-12-26 22:50:22 +0300

    add new file

---
+++ Final_Project/settings.py
@@ -0,0 +1,33 @@
+class AuthEmail:
+    email = 'liliashad@mail.ru'
+    password = 'Samsung1#'
+
+
+class AuthPhone:
+    phone = '9133350465'
+    password = 'Samsung1#'
+
+
+class AuthLogin:
+    login = 'hfusj_175847555'
+    password = 'Samsung1#'
+
+
+class InvalidData:
+    login = 'login_impostor'
+    password = "Qwerty12345"
+
+
+class SymbolData:
+    login = '/*!@#$'
+    password = "Qwerty12345"
+
+
+class KirillData:
+    login = 'Кириллица'
+    password = "Кириллица"
+
+
+class BIGData:
+    login = 'login'
+    password = "passw"
